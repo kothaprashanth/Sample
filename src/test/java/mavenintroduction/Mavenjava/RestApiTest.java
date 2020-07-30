b3ff93ce6e110d7a319bc0a8c4329406f308fc94
dell <dell@DESKTOP-LFRK7HV>
2020-07-31 04:14:01 +0800

    firstcommit

---
+++ src/test/java/mavenintroduction/Mavenjava/RestApiTest.java
@@ -0,0 +1,20 @@
+package mavenintroduction.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	
+	@Test
+	public void postJeera()
+	{
+		System.out.println("postJeera");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
---
+++ src/test/java/mavenintroduction/Mavenjava/SeleniumTest.java
@@ -0,0 +1,22 @@
+package mavenintroduction.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomation");
+		
+	}
+	
+	@Test
+	public void elementUi()
+	{
+		
+		System.out.println("elementUi");	
+	}
+}
